bfc70bf295dec03373c653573de6b6326a64d360
Cuong  Truong <cuong@cakedefi.com>
2023-10-20 15:41:42 +0800

    add other files

--- test/OracleConsumer.spec.ts
+++ test/OracleConsumer.spec.ts
@@ -1,8 +1,8 @@
 import { ethers } from "hardhat";
-import {DataServiceWrapper} from "@redstone-finance/evm-connector/dist/src/wrappers/DataServiceWrapper";
 import { ethers as ethersv5 } from "ethersv5.7.2";
 import {abi as RedstoneOracleAbi} from "../artifacts/contracts/RedstoneOracle.sol/RedstoneOracle.json"
-import {expect} from "chai";
+import {abi as OracleConsumerAbi} from "../artifacts/contracts/OracleConsumer.sol/OracleConsumer.json"
+import { CustomDataServiceWrapper } from "../scripts/CustomDataServiceWrapperClass";
 
 describe("Oracle consumer", function () {
 
@@ -11,16 +11,17 @@ describe("Oracle consumer", function () {
     const redstoneOracleAddress = await redstoneOracle.getAddress();
     const oracleConsumer = await ethers.deployContract("OracleConsumer", [[ethers.ZeroAddress], [ethers.encodeBytes32String("ETH")], redstoneOracleAddress]);
     const redstoneOracleV5 = new ethersv5.Contract(redstoneOracleAddress, RedstoneOracleAbi, new ethersv5.providers.JsonRpcProvider("http://127.0.0.1:8545"));
-    const redstonePayload = await (new DataServiceWrapper({
+    const oracleConsumerV5 = new ethersv5.Contract(await oracleConsumer.getAddress(),OracleConsumerAbi,  new ethersv5.providers.JsonRpcProvider("http://127.0.0.1:8545") )
+
+    const oracleConsumerV5wrapped = new CustomDataServiceWrapper({
       dataServiceId: "redstone-primary-prod",
       dataFeeds: ["ETH"]
-    })).getRedstonePayloadForManualUsage(redstoneOracleV5);
+    }, 
+      redstoneOracleV5
+    ).overwriteEthersContract(oracleConsumerV5);
 
-    expect(BigInt(await ((await ethers.getSigners())[0]).call(
-      {
-        data: oracleConsumer.interface.encodeFunctionData("getPriceFromOracle", [ethers.ZeroAddress]) + ethers.AbiCoder.defaultAbiCoder().encode(["bytes"],[redstonePayload]).slice(66),
-        to: await oracleConsumer.getAddress(),
-      }))).to.gt(0n);
-
+    const result = await oracleConsumerV5wrapped.getPriceFromOracle(ethers.ZeroAddress);
+    console.log(result);
+  
   })
 });
