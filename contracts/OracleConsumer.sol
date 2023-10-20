dba992ded8061a634c79be70f23791dc81fdfff4
Cuong  Truong <cuong@cakedefi.com>
2023-10-20 15:29:24 +0800

    add new way to fetch price in the smart contract

--- contracts/OracleConsumer.sol
+++ contracts/OracleConsumer.sol
@@ -12,18 +12,41 @@ contract OracleConsumer is Ownable {
         oracleContract = RedstoneOracle(_oracleContract);
     }
 
-    function getPriceFromOracle(address _tokenAddress) external view returns (uint256) {
+    function mockSwap(address firstToken, address secondToken ) public view returns(uint256) {
+        return getPriceFromOracle(address(0));
+    }
+
+    function getPriceFromOracle(address _tokenAddress) public view returns (uint256) 
+    {
         bytes32 dataFeedId = tokenToDataFeedId[_tokenAddress];
-        uint256 payloadSize;
+        uint256 oracleCallDataSize;
+        address oracleContractAddress = address(oracleContract);
+        
         assembly {
-            payloadSize := calldataload(0x24)
+            oracleCallDataSize := add(calldatasize(), 0x24)
         }
 
-        bytes memory redstonePayload = new bytes(payloadSize);
+        bytes memory redstonePayload = new bytes(oracleCallDataSize);
+    
         assembly {
-            calldatacopy(add(redstonePayload, 0x20), 0x44, payloadSize)
+            mstore8(add(redstonePayload, 0x20), 0xb2)
+            mstore8(add(redstonePayload, 0x21), 0xee)
+            mstore8(add(redstonePayload, 0x22), 0x2f)
+            mstore8(add(redstonePayload, 0x23), 0x01)
+            mstore(add(redstonePayload, 0x24), dataFeedId)
+            calldatacopy(add(redstonePayload, 0x44), 0x00, sub(oracleCallDataSize, 0x24))
+            let oldMemorySize := mload(0x40)
+            let success := staticcall(gas(), oracleContractAddress, add(redstonePayload, 0x20), oracleCallDataSize, 0, 0)
+            returndatacopy(oldMemorySize, 0, returndatasize())
+            mstore(0x40, add(oldMemorySize, returndatasize()))
+            switch success
+            case 0  {
+                revert(oldMemorySize, returndatasize())
+            }
+            default {
+                return(oldMemorySize, returndatasize())
+            }
         }
-        
-        return oracleContract.getLatestPriceWithPayload(dataFeedId, redstonePayload);
+
     } 
 }
