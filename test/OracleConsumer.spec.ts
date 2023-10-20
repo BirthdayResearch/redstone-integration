563f98d9903db0950bcae27e695fdd517cd92c33
Cuong  Truong <cuong@cakedefi.com>
2023-10-20 16:56:46 +0800

    first commit for main

--- test/OracleConsumer.spec.ts
+++ test/OracleConsumer.spec.ts
@@ -1,26 +1,40 @@
 import { ethers } from "hardhat";
-import {DataServiceWrapper} from "@redstone-finance/evm-connector/dist/src/wrappers/DataServiceWrapper";
 import { ethers as ethersv5 } from "ethersv5.7.2";
-import {abi as RedstoneOracleAbi} from "../artifacts/contracts/RedstoneOracle.sol/RedstoneOracle.json"
+import {abi as oracleConsumerAbi} from "../artifacts/contracts/ExampleOracleConsumer.sol/ExampleOracleConsumer.json"
 import {expect} from "chai";
+import { WrapperBuilder } from "@redstone-finance/evm-connector";
 
 describe("Oracle consumer", function () {
 
   it("Should get the price", async () => {
-    const redstoneOracle = await ethers.deployContract("RedstoneOracle");
-    const redstoneOracleAddress = await redstoneOracle.getAddress();
-    const oracleConsumer = await ethers.deployContract("OracleConsumer", [[ethers.ZeroAddress], [ethers.encodeBytes32String("ETH")], redstoneOracleAddress]);
-    const redstoneOracleV5 = new ethersv5.Contract(redstoneOracleAddress, RedstoneOracleAbi, new ethersv5.providers.JsonRpcProvider("http://127.0.0.1:8545"));
-    const redstonePayload = await (new DataServiceWrapper({
-      dataServiceId: "redstone-primary-prod",
-      dataFeeds: ["ETH"]
-    })).getRedstonePayloadForManualUsage(redstoneOracleV5);
+    const oracleConsumer = await ethers.deployContract("ExampleOracleConsumer");
+    const oracleConsumerV5 = new ethersv5.Contract(await oracleConsumer.getAddress(), oracleConsumerAbi, new ethersv5.providers.JsonRpcProvider("http://127.0.0.1:8545"));
+    const wrappedConsumerForETH = WrapperBuilder.wrap(oracleConsumerV5).usingDataService({
+      dataFeeds: ["ETH"],
+      dataServiceId: "redstone-primary-prod"
+    })
 
-    expect(BigInt(await ((await ethers.getSigners())[0]).call(
-      {
-        data: oracleConsumer.interface.encodeFunctionData("getPriceFromOracle", [ethers.ZeroAddress]) + ethers.AbiCoder.defaultAbiCoder().encode(["bytes"],[redstonePayload]).slice(66),
-        to: await oracleConsumer.getAddress(),
-      }))).to.gt(0n);
+    expect((await wrappedConsumerForETH.getLatestPrice(ethers.encodeBytes32String("ETH"))).gt(0)).to.be.true;
+
+    expect((await wrappedConsumerForETH.getLatestEthPrice()).gt(0)).to.be.true;
+
+    const wrappedConsumerForBTC = WrapperBuilder.wrap(oracleConsumerV5).usingDataService({
+      dataFeeds: ["BTC"],
+      dataServiceId: "redstone-primary-prod"
+    })
+
+    expect((await wrappedConsumerForBTC.getLatestPrice(ethers.encodeBytes32String("BTC"))).gt(0)).to.be.true;
+
+    const wrappedConsumerForBTCETH = WrapperBuilder.wrap(oracleConsumerV5).usingDataService({
+      dataFeeds: ["BTC", "ETH"],
+      dataServiceId: "redstone-primary-prod"
+    })
+
+    const [BTCPrice, ETHPrice] = await wrappedConsumerForBTCETH.getLatestPrices([ethers.encodeBytes32String("BTC"), ethers.encodeBytes32String("ETH")]);
+
+    expect(BTCPrice.gt(0)).to.be.true;
+    expect(ETHPrice.gt(0)).to.be.true;
+
 
   })
 });
