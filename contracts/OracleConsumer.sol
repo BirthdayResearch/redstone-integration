5b8f8c16642a6fc5799988888c8ef4b698dced59
Cuong  Truong <cuong@cakedefi.com>
2023-10-23 16:53:47 +0800

    change to getting multiple prices

--- contracts/OracleConsumer.sol
+++ contracts/OracleConsumer.sol
@@ -12,39 +12,39 @@ contract OracleConsumer is Ownable {
         oracleContract = RedstoneOracle(_oracleContract);
     }
 
-    function mockSwap(address firstToken, address secondToken ) public view returns(uint256) {
-        return getPriceFromOracle(address(0));
+    function mockSwap(address firstToken, address secondToken) public view returns(uint256[] memory) {
+        address[] memory tokenAddresses = new address[](1);
+        tokenAddresses[0] = firstToken;
+        return getPricesFromOracle(tokenAddresses);
     }
 
-    function getPriceFromOracle(address _tokenAddress) public view returns (uint256) 
+    function getPricesFromOracle(address[] memory _tokenAddresses) public view returns (uint256[] memory) 
     {
-        bytes32 dataFeedId = tokenToDataFeedId[_tokenAddress];
-        uint256 oracleCallDataSize;
-        address oracleContractAddress = address(oracleContract);
+        bytes32[] memory dataFeedIds = new bytes32[](_tokenAddresses.length);
+        for (uint i = 0; i < _tokenAddresses.length; i++) {
+            dataFeedIds[i] = tokenToDataFeedId[_tokenAddresses[i]];
+        }
+        address oracleContractAddress = address(oracleContract);        
+        bytes memory redstonePayload = abi.encodeWithSignature("getLatestPrices(bytes32[])", dataFeedIds);
         
         assembly {
-            oracleCallDataSize := add(calldatasize(), 0x24)
-        }
-
-        bytes memory redstonePayload = new bytes(oracleCallDataSize);
-    
-        assembly {
-            mstore8(add(redstonePayload, 0x20), 0xb2)
-            mstore8(add(redstonePayload, 0x21), 0xee)
-            mstore8(add(redstonePayload, 0x22), 0x2f)
-            mstore8(add(redstonePayload, 0x23), 0x01)
-            mstore(add(redstonePayload, 0x24), dataFeedId)
-            calldatacopy(add(redstonePayload, 0x44), 0x00, sub(oracleCallDataSize, 0x24))
+            let initialRedstoneLen := mload(redstonePayload)
+            let newRedstoneLen := add(initialRedstoneLen, calldatasize())
+            mstore(redstonePayload, newRedstoneLen)
             let oldMemorySize := mload(0x40)
-            let success := staticcall(gas(), oracleContractAddress, add(redstonePayload, 0x20), oracleCallDataSize, 0, 0)
-            returndatacopy(oldMemorySize, 0, returndatasize())
-            mstore(0x40, add(oldMemorySize, returndatasize()))
+            let newMemorySizeAfterAddingData
+            newMemorySizeAfterAddingData := add(oldMemorySize, calldatasize())
+            calldatacopy(add(redstonePayload, add(initialRedstoneLen, 0x20)), 0x00, calldatasize())
+            let success := staticcall(gas(), oracleContractAddress, add(redstonePayload, 0x20), newRedstoneLen, 0, 0)
+            returndatacopy(newMemorySizeAfterAddingData, 0, returndatasize())
+            // memory expand because of adding return data to memory
+            mstore(0x40, add(newMemorySizeAfterAddingData, returndatasize()))
             switch success
             case 0  {
-                revert(oldMemorySize, returndatasize())
+                revert(newMemorySizeAfterAddingData, returndatasize())
             }
             default {
-                return(oldMemorySize, returndatasize())
+                return(newMemorySizeAfterAddingData, returndatasize())
             }
         }
 
--- test/OracleConsumer.spec.ts
+++ test/OracleConsumer.spec.ts
@@ -9,18 +9,18 @@ describe("Oracle consumer", function () {
   it("Should get the price", async () => {
     const redstoneOracle = await ethers.deployContract("RedstoneOracle");
     const redstoneOracleAddress = await redstoneOracle.getAddress();
-    const oracleConsumer = await ethers.deployContract("OracleConsumer", [[ethers.ZeroAddress], [ethers.encodeBytes32String("ETH")], redstoneOracleAddress]);
+    const oracleConsumer = await ethers.deployContract("OracleConsumer", [[ethers.ZeroAddress, `0x`+ "0".repeat(39) + `1`], [ethers.encodeBytes32String("ETH"), ethers.encodeBytes32String("BTC")], redstoneOracleAddress]);
     const redstoneOracleV5 = new ethersv5.Contract(redstoneOracleAddress, RedstoneOracleAbi, new ethersv5.providers.JsonRpcProvider("http://127.0.0.1:8545"));
     const oracleConsumerV5 = new ethersv5.Contract(await oracleConsumer.getAddress(),OracleConsumerAbi,  new ethersv5.providers.JsonRpcProvider("http://127.0.0.1:8545") )
 
     const oracleConsumerV5wrapped = new CustomDataServiceWrapper({
       dataServiceId: "redstone-primary-prod",
-      dataFeeds: ["ETH"]
+      dataFeeds: ["ETH", "BTC"]
     }, 
       redstoneOracleV5
     ).overwriteEthersContract(oracleConsumerV5);
 
-    const result = await oracleConsumerV5wrapped.getPriceFromOracle(ethers.ZeroAddress);
+    const result = await oracleConsumerV5wrapped.getPricesFromOracle([ethers.ZeroAddress, `0x`+ "0".repeat(39) + `1`]);
     console.log(result);
   
   })
