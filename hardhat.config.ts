f2717e288234d827e6b2916596462dae5bec4374
Cuong  Truong <cuong@cakedefi.com>
2023-11-23 12:23:14 +0800

    add necessary things for deployment of redstone oracle

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,52 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
+import "@nomicfoundation/hardhat-ledger";
+
+require("dotenv").config({
+  path: ".env"
+})
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   paths: {
     sources: "contracts/"
+  },
+  networks: {
+    DMCTestnet: {
+      url: process.env.DMC_TESTNET_URL || '', 
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [], 
+      chainId: 1131
+    }, 
+    DMCMainnet: {
+      url: process.env.DMC_MAINNET_URL || '', 
+      // ledgerAccounts: ['first EVM address of your ledger']
+      // chainId: 
+    }
+  }, 
+  etherscan: {
+    apiKey: {
+      DMCTestnet: 'abc', 
+      // DMCMainnet: 'abc'
+    }, 
+    customChains: [
+      {
+        network: 'DMCTestnet', 
+        chainId: 1131, 
+        urls: {
+          apiURL: 'https://blockscout.testnet.ocean.jellyfishsdk.com/api', 
+          browserURL: 'https://meta.defiscan.live/?network=TestNet'
+        }
+      }, 
+      // {
+      //   network: 'DMCMainnet', 
+      //   chainId: 
+      //   urls : {
+      //     apiURL: blockscout URL, 
+      //     browserURL: browserURL
+      //  }
+      // }
+    ]
   }
 };
 
